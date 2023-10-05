d6a0cfbee5ba847e544f9a8a7e9e20dd8b14e08c
ubuntunux <ubuntunux@gmail.com>
2023-10-05 09:04:34 +0900

    update

---
+++ app/app.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import traceback
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.clock import Clock
+from kivy.metrics import Metrics
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+from kivy.uix.vkeyboard import VKeyboard
+from kivy.logger import Logger
+
+from app.constants import *
+from utility.kivy_helper import *
+from utility.singleton import SingletonInstane
+
+
+class MyApp(App, SingletonInstane):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        Logger.info(f'Run: {self.__name__}')
+
+        self.screen_helper = None
+        self.screen = None
+        
+    def destroy(self):
+        pass
+
+    def on_stop(self):
+        self.destroy()
+        Config.write()
+
+    def build(self):
+        # Window.maximize()
+        Window.softinput_mode = 'below_target'
+        # keyboard_mode: '', 'system', 'dock', 'multi', 'systemanddock', 'systemandmulti'
+        Config.set('kivy', 'keyboard_mode', 'system')
+        Window.configure_keyboards()
+
+        self.root = Widget()
+        self.screen_helper = ScreenHelper(size=Window.size)
+        self.root.add_widget(self.screen_helper.screen_manager)
+        self.screen = Screen(name=self.__name__)
+        self.screen_helper.add_screen(self.screen)
+        self.screen_helper.current_screen(self.screen)
+
+        layout = BoxLayout(orientation='vertical', size=(1, 1))
+        self.screen.add_widget(layout)
+        btn = Button()
+        layout.add_widget(self.output_scroll_view)
+
+        Clock.schedule_interval(self.update, 0)
+        return self.root
+
+    def first_update(self):
+        pass
+
+    def update(self, dt):
+        if self.is_first_update:
+            self.first_update()
+            self.is_first_update = False
+
--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 from glob import glob
 import os
 
-from javis.constants import *
+from app.constants import *
 
-def run_app():     
-    log_folder = os.path.abspath('.log')
-    log_maxfiles = 10
-
-    # Note: Config must be set before import kivy.logger
-    from kivy.config import Config
-    Config.read(javis_config_file)
-    Config.set('kivy', 'log_level', 'info')
-    Config.set('kivy', 'log_enable', 1)
-    Config.set('kivy', 'log_name', '%Y%m%d_%H%M%S_%_.log')
-    Config.set('kivy', 'log_dir', log_folder)
-    Config.set('kivy', 'log_maxfiles', log_maxfiles)
-    Config.write()
-
-    # clear old log
-    logs = list(glob("{}/*.log".format(log_folder)))
-    logs.sort()
-    log_count = len(logs)
-    if log_maxfiles <= log_count:
-        remove_count = log_count - log_maxfiles + 1
-        for log_file in logs[:remove_count]:
-            os.remove(log_file)
-
+def run():
     try:
         # android permission
         from kivy.utils import platform
@@ -34,15 +12,34 @@ def run_app():
             request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
     except:
         pass
+          
+    # Note: Config must be set before import kivy.logger
+    from kivy.config import Config
+    Config.read(CONFIG_FILE)
+    Config.set('kivy', 'log_level', 'info')
+    Config.set('kivy', 'log_enable', 1)
+    Config.set('kivy', 'log_name', '%Y%m%d_%H%M%S_%_.log')
+    Config.set('kivy', 'log_dir', LOG_FOLDER)
+    Config.set('kivy', 'log_maxfiles', MAX_LOG_NUM)
+    Config.write()
+
+    # clear old log
+    logs = list(glob("{}/*.log".format(LOG_FOLDER)))
+    logs.sort()
+    log_count = len(logs)
+    if MAX_LOG_NUM <= log_count:
+        remove_count = log_count - MAX_LOG_NUM + 1
+        for log_file in logs[:remove_count]:
+            os.remove(log_file)
 
     # run app
     from kivy.logger import Logger
-    from javis.javis import JavisApp
-    javis_app = JavisApp.instance()
-    javis_app.run()
+    from app.app import MyApp
+    my_app = MyApp.instance()
+    my_app.run()
     Logger.info("Bye")
     Config.write()
 
 
 if __name__ == '__main__':
-    run_app()
+    run()
