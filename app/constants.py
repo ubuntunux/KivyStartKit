6c97e132e2ceb45bb592f4a33a96fe44dda19535
ubuntunux <ubuntunux@gmail.com>
2023-10-19 07:54:22 +0900

    update

--- app/constants.py
+++ app/constants.py
@@ -1,9 +1,9 @@
 import os
 
 DATA_FOLDER = 'data'
-FONT_NAME = 'fonts/NanumGothic_Coding.ttf'
-OUTPUT_FILE = os.path.join(DATA_FOLDER, 'output.log')
+FONT_NAME = os.path.join(DATA_FOLDER, 'fonts/NanumGothic_Coding.ttf')
+OUTPUT_FILE = os.path.join(DATA_FOLDER, 'output/output.log')
 CONFIG_FILE = os.path.join(DATA_FOLDER, 'config.ini')
-LOGO_FILE = os.path.join(DATA_FOLDER, "logo_image.png")
+LOGO_FILE = os.path.join(DATA_FOLDER, "icons/logo_image.png")
 LOG_FOLDER = os.path.abspath('.log')
 MAX_LOG_NUM = 10
--- javis/constants.py
+++ javis/constants.py
@@ -2,9 +2,9 @@ import os
 
 data_folder = 'data'
 
-app_font_name = 'fonts/NanumGothic_Coding.ttf'
-javis_output_file = os.path.join(data_folder, 'output.log')
+app_font_name = os.path.join(data_folder, 'fonts/NanumGothic_Coding.ttf')
+javis_output_file = os.path.join(data_folder, 'output/output.log')
 javis_config_file = os.path.join(data_folder, 'config.ini')
-logo_file = os.path.join(data_folder, "icon.png")
+logo_file = os.path.join(data_folder, "icons/icon.png")
 section_listener = 'listener'
 config_listener_pos = section_listener, 'pos'
