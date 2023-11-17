dc1c7e04fabda700d343852702809b4cd561391b
ubuntunux <ubuntunux@gmail.com>
2023-11-17 21:30:06 +0900

    debug labdl

--- utility/kivy_widgets.py
+++ utility/kivy_widgets.py
@@ -1,3 +1,4 @@
+from kivy.clock import Clock
 from kivy.uix.textinput import TextInput
 
 class KivyLabel(TextInput):
@@ -18,3 +19,31 @@ class KivyLabel(TextInput):
             kargs["foreground_color"] = (1,1,1,1)
             
         super(KivyLabel, self).__init__(**kargs)
+
+
+class DebugLabel(KivyLabel):
+    def __init__(self, show_fps=True, display_count=5, display_time=3.0,**kargs):
+        super(DebugLabel, self).__init__(**kargs)
+        self.show_fps = show_fps
+        self.display_count = display_count
+        self.display_time = display_time
+        self.debug_text = []
+        
+    def debug_print(self, text):
+        self.remained_display_time = self.display_time
+        if self.display_count < len(self.debug_text):
+            self.debug_text.pop()
+        self.debug_text.insert(0, text)
+        
+    def update(self, dt):
+        fps = Clock.get_fps()
+        time = 1000.0 / fps if 0 < fps else 0
+        self.text = f"fps: {format(fps, '0.2f')}\ntime(ms): {format(time, '0.2f')}"
+        if self.debug_text:
+            self.text += "\n"
+            self.text += "\n".join(self.debug_text)
+            self.remained_display_time -= dt
+            if self.remained_display_time < 0.0:
+                self.debug_text = []
+        self.height = self.minimum_height
+        
