cd03bf5f89d98c289ce972cf202d5145c64da1bd
ubuntunux <ubuntunux@gmail.com>
2024-01-13 02:20:41 +0900

    bug fix

--- core/platform/__init__.py
+++ core/platform/__init__.py
@@ -4,7 +4,4 @@ from kivy import platform
 from .default import BasePlatformAPI
 
 def get_platform_api():
-    if platform == 'android':
-        from .android import AndroidPlatformAPI
-        return AndroidPlatformAPI.instance()
     return BasePlatformAPI.instance()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from glob import glob
+import kivy
 from core.main_app import MainApp
 
 if __name__ == '__main__':
     main_app = MainApp.instance("Kivy Start Kit")
     main_app.run()
+    
